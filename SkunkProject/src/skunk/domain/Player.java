a35016f3c01471304cab479168b77da5f3387ff7
noniv <nonivhm@gmail.com>
2020-10-23 16:40:29 -0500

    Update Player class

--- SkunkProject/src/skunk/domain/Player.java
+++ SkunkProject/src/skunk/domain/Player.java
@@ -4,11 +4,29 @@ public class Player {
 	
 	private String name;
 	
+	// only one turn at this time
+	private Turn curTurn;
+	
+	private int totalScores;
+	
 	public Player(String name) {
 		this.name = name;
+		curTurn = new Turn();
 	}
 	
 	public String getName() {
 		return this.name;
+	}
+	
+	public void play() {
+		curTurn.createRoll(new Dice());
+	}
+	
+	public void stop() {
+		totalScores += curTurn.getTurnScores();
+	}
+	
+	public int getTotalScores() {
+		return totalScores;
 	}
 }
