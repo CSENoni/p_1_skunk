980c11c31f46326fc16585f1353a3889ed02277a
noniv <nonivhm@gmail.com>
2020-10-23 16:32:22 -0500

    Update TurnTest

--- SkunkProject/test/skunk/domain/TestTurn.java
+++ SkunkProject/test/skunk/domain/TestTurn.java
@@ -14,5 +14,18 @@ class TestTurn {
 		turn.createRoll(new Dice());
 		assertTrue(turn.numbersOfRolls() > 1);
 	}
+	
+	@Test
+	void testScores() {
+		Dice dice = new Dice();
+		Turn turn = new Turn();
+		turn.createRoll(dice);
+		int scores = dice.getLastRoll();
+		assertTrue(turn.getTurnScores() == scores);
+		dice = new Dice();
+		turn.createRoll(dice);
+		scores += dice.getLastRoll();
+		assertTrue(turn.getTurnScores() == scores);
+	}
 
 }
