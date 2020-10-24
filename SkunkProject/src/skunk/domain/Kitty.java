c670179f5b9db50b33e98664649e58d55200ec0e
noniv <nonivhm@gmail.com>
2020-10-23 19:01:23 -0500

    Update Kitty and TestKitty

--- SkunkProject/src/skunk/domain/Kitty.java
+++ SkunkProject/src/skunk/domain/Kitty.java
@@ -3,8 +3,12 @@ package skunk.domain;
 public class Kitty {
 	private int chip;
 	
-	public void add() {
-		chip++;
+	public void add(int num) {
+		chip += num;
+	}
+	
+	public void chipDect() {
+		chip -= 5;
 	}
 	
 	public int getChip() {
--- SkunkProject/test/skunk/domain/TestKitty.java
+++ SkunkProject/test/skunk/domain/TestKitty.java
@@ -7,10 +7,30 @@ import org.junit.jupiter.api.Test;
 class TestKitty {
 
 	@Test
-	void addChip() {
+	void testAddChip() {
 		Kitty kit = new Kitty();
 		
 		assertTrue(kit.getChip() == 0);
+		
+		kit.add(3);
+		
+		assertTrue(kit.getChip() == 3);
 	}
 
+	@Test
+	void testChipDect() {
+		Kitty kit = new Kitty();
+		
+		kit.add(50);
+		kit.chipDect();
+		assertTrue(kit.getChip() == 45);
+	}
+	
+	@Test
+	void testGetChip() {
+		Kitty kit = new Kitty();
+		
+		kit.add(50);
+		assertTrue(kit.getChip() == 50);
+	}
 }
